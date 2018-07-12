edcdaf81b0f72e0d24777c47007bf279cfa7c48b
konradp <41149635+konradp@users.noreply.github.com>
2018-07-12 12:28:30 +0200

    Add Existing Test Project

---
+++ Android_Test/Assets/Scripts/Ball.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    Rigidbody2D rb;
+    float x, y;
+    Vector3 bounced;
+    public float ballSpeed = 60f;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        x = Random.Range(-3f,3f)* ballSpeed;
+        y = Random.Range(-3f, 3f) * ballSpeed;
+        bounced = new Vector3(x, y);
+        rb.AddForce(bounced);
+	}
+	
+	void Update () {
+
+    }
+    void SwitchPosition(int scenario)
+    {
+        Debug.Log(bounced);
+        int temp = (int)x;
+        switch (scenario)
+        {
+            case (0):
+                bounced = Vector3.Reflect(this.transform.position, Vector3.up);
+                break;
+            case (1):
+                bounced = Vector3.Reflect(this.transform.position, Vector3.right);
+                break;
+            case (2):
+                bounced = Vector3.Reflect(this.transform.position, Vector3.down);
+                break;
+            case (3):
+                bounced = Vector3.Reflect(this.transform.position, Vector3.left);
+                break;
+            default:
+                break;
+        }
+        bounced *= ballSpeed;
+        Debug.Log(bounced);
+        rb.Sleep();
+        rb.WakeUp();
+        rb.AddForce(bounced);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Wall")
+        {
+            int check = collision.GetComponent<Wall>().placement;
+            SwitchPosition(check);
+        }
+    }
+}
---
+++ Android_Test/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+    //value to store mouse position when clicked
+    Vector2 mousePos;
+    //use for checking if we're gonna place walls horizontally or vertically. 0 - hor, 1 - vert
+    int pos;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
